df853bbdb514c3fe04cd06f2140e519e5160dba5
LimJinOuk <jinouk038@gmail.com>
2026-07-21 08:59:19 +0900

    first commit

---
+++ src/main/java/com/example/codegate/CodeGateApplication.java
@@ -0,0 +1,13 @@
+package com.example.codegate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeGateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeGateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/codegate/CodeGateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codegate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeGateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
